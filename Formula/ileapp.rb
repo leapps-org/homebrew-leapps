51cd61a560cbcc6061fa9edb7057f70fcd4d8841
Johann POLEWCZYK <johannplw@me.com>
2024-12-21 13:31:00 +0100

    Update sha256 in ileapp-gui.rb and ileapp.rb

--- Formula/ileapp.rb
+++ Formula/ileapp.rb
@@ -4,10 +4,10 @@ class Ileapp < Formula
 
   if Hardware::CPU.intel?
     url "https://github.com/abrignoni/iLEAPP/releases/download/v2.0.2/ileapp-v2.0.2-Mac_Intel.zip"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "1a2fc616f48a5499ae04b0c5d9beab0912b278d3b09bf3e5ce0650d67394e5c8"
   else
     url "https://github.com/abrignoni/iLEAPP/releases/download/v2.0.2/ileapp-v2.0.2-Apple_Silicon.zip"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "4bba124a84e475cae32241223714024ee121ed8579a82ac4ae2a8b7e71743e7c"
   end
 
   def install
